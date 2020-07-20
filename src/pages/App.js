772e39d34fb4ca4f2cc7044217ec66a2880c4c34
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-20 16:46:31 -0500

    change path to Index

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Usuarios from '../components/Usuarios';
+import '../styles/styles.scss'
 import PokeCards from '../pages/PokeCards';
 import Home from '../pages/Home';
 
@@ -13,8 +14,8 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/index/cards' component={ PokeCards } />
-      <Route exact path='/index' component={ Home } />
-      <Route exact path='/' component={ Usuarios } />
+      <Route exact path='/' component={ Home } />
+      {/* <Route exact path='/' component={ Usuarios } /> */}
       <Route exact path='/tareas' component={ Tareas } />
     </Switch>
 
