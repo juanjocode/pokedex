af376dedac8c7d65b5ac959cea7707b91b0b1066
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-20 18:23:47 -0500

    se crea el archivo para la biblioteca de imagenes

---
+++ src/img/images.js
@@ -0,0 +1,23 @@
+var images = {
+    bug: '../img/types/bug.svg',
+    dark: './dark.svg',
+    dragon: './dragon.svg',
+    electric: './electric.svg',
+    fairy: './fairy.svg',
+    fight: './fight.svg',
+    fire: './fire.svg',
+    flying: './flying.svg',
+    ghost: './ghost.svg',
+    grass: './grass.svg',
+    ground: './ground.svg',
+    ice: './ice.svg',
+    normal: './normal.svg',
+    poison: './poison.svg',
+    psychic: './psychic.svg',
+    rock: './rock.svg',
+    steel: './steel.svg',
+    water: './water.svg',
+
+}
+
+export default images;
