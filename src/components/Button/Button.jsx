66c968fd02390aec95c5af2afc82577743639c99
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-21 14:22:16 -0500

    add tooltip to button cmps

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default class PokeButton extends Component {
 
     render() {
-        const { icon, title, link, className, classNameContainer, style, onClick } = this.props;
+        const { icon, title, link, className, classNameContainer, style, onClick, tooltip } = this.props;
+
+        console.log("TTOOOLLLLLL  " + tooltip);
+
+    //    if (tooltip === undefined) {
+    //     //    alert('¿cosa')
+    //     document.getElementsByClassName('MuiTooltip-tooltip').style.color = 'blue';
+           
+    //    }
+
+
         return (
-            <a href={link} className={classNameContainer}>
-                <button style={style} className={`poke-button ${className}`} onClick={onClick}>
-                    <img src={icon} alt={icon} />
-                    <p>{title}</p>
-                </button>
+            <a href={link} className={`${classNameContainer} button-tooltip`}>
+
+                
+                    <button style={style} className={`poke-button ${className}`} onClick={onClick}>
+                    <Tooltip title={tooltip}  >
+                        <img src={icon} alt={icon} />
+                </Tooltip>
+                        <p>{title}</p>
+                    </button>
             </a>
         );
     }
