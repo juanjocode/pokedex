ca52473695bc554dacf106ba9024bd9d69ee19bf
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-20 21:32:39 -0500

    add buttons menu

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ export default class PokeButton extends Component {
         const { icon, title, link, className, style, onClick } = this.props;
         return (
             <a href={link}>
-                <button style={{style}} className={`poke-button ${className}`} onClick={onClick}>
+                <button style={style} className={`poke-button ${className}`} onClick={onClick}>
                     <img src={icon} alt={icon} />
                     <p>{title}</p>
                 </button>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,6 +6,7 @@ import iconStrong from '../../img/icon-strongest.svg'
 import iconWeakness from '../../img/icon-weakness.svg'
 import iconLegendary from '../../img/icon-legendary.svg'
 import iconReset from '../../img/icon-reset.svg'
+import iconCollapse from '../../img/icon-collapse.svg'
 
 const Menu = () => {
   return (
@@ -19,35 +20,39 @@ const Menu = () => {
           icon={iconStrong}
           title="Stronguest"
           link="/index/cards"
+          style={{width:"100%"}}
           className=""
         />
         <PokeButton
           icon={iconWeakness}
           title="Weakness"
           link="/index/cards"
+           style={{width:"100%"}}
           className=""
         />
         <PokeButton
           icon={iconLegendary}
           title="Legendaries"
           link="/index/cards"
+           style={{width:"100%"}}
           className=""
         />
         <PokeButton
           icon={iconReset}
           title="Reset / All"
           link="/index/cards"
+           style={{width:"100%"}}
           className=""
         />
+        <Pokeslider />
         
         <PokeButton
-          icon={iconReset}
+          icon={iconCollapse}
           title="Reset / All"
           link="/index/cards"
           className=""
         />
 
-        <Pokeslider />
 
 
       </div>
