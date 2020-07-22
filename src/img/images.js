7ba87a552761d408d827ad2262aa02b9901a7b99
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-22 11:06:48 -0500

    fix url in images library

--- src/img/images.js
+++ src/img/images.js
@@ -1,23 +1,31 @@
-var images = {
-    bug: 'types/bug.svg',
-    dark: './dark.svg',
-    dragon: './dragon.svg',
-    electric: './electric.svg',
-    fairy: './fairy.svg',
-    fight: './fight.svg',
-    fire: './fire.svg',
-    flying: './flying.svg',
-    ghost: './ghost.svg',
-    grass: './grass.svg',
-    ground: './ground.svg',
-    ice: './ice.svg',
-    normal: './normal.svg',
-    poison: './poison.svg',
-    psychic: './psychic.svg',
-    rock: './rock.svg',
-    steel: './steel.svg',
-    water: './water.svg',
+const images = {
+    bug: require('./types/bug.svg'),
+    dark: require( './types/dark.svg'),
+    dragon: require( './types/dragon.svg'),
+    electric: require( './types/electric.svg'),
+    fairy: require( './types/fairy.svg'),
+    fight: require( './types/fight.svg'),
+    fire: require( './types/fire.svg'),
+    flying: require( './types/flying.svg'),
+    ghost: require( './types/ghost.svg'),
+    grass: require( './types/grass.svg'),
+    ground: require( './types/ground.svg'),
+    ice: require( './types/ice.svg'),
+    normal: require( './types/normal.svg'),
+    poison: require( './types/poison.svg'),
+    psychic: require( './types/psychic.svg'),
+    rock: require( './types/rock.svg'),
+    steel: require( './types/steel.svg'),
+    water: require( './types/water.svg'),
+    default: require( './types/water.svg'),
+}
 
+export const getType = (type) => {
+    let typePokemon = images[type];
+    if (typePokemon === undefined) {
+        typePokemon = images.default
+    }
+    return typePokemon;
 }
 
 export default images;
