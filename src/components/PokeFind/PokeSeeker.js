bd560fd16d0c2c8ffecb19a6c1e4e56184a97e14
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-23 16:41:23 -0500

    add styles to pokeFind

--- src/components/PokeFind/PokeSeeker.js
+++ src/components/PokeFind/PokeSeeker.js
@@ -1,45 +1,58 @@
-/* eslint-disable no-use-before-define */
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-// import {GlobalState} from '../Providers/GlobalState';
-// import PokeViews from '../components/PokeViews';
-// import PokeSlider from '../components/PokeSlider'
+import Tooltip from '@material-ui/core/Tooltip';
+import iconFind from '../../img/icon-search.svg';
+
+
+const MyTooltip = ({title, children}) =>{
+  if (title === undefined) {
+      return children;
+  }
+  return (
+  <Tooltip title={title}>
+      {children}
+  </Tooltip>
+  )
+}
+
 class PokeSeeker extends React.Component {
 
   constructor(props) {
     super(props);
-
-    
 
     this.state = {
       pokemon: []
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     // fetch('https://jsonplaceholder.typicode.com/users')
     fetch('http://localhost:3000/pokemon')
       .then(response => response.json())
       .then(initialData => this.setState({ pokemon: initialData }))
   }
 
-  render () {
+  render() {
+
+    const { handleClick, tooltip } = this.props;
     return (
-      <>
-            <Autocomplete
-            id="free-solo-demo"
-            freeSolo
-            disableClearable
-            onChange={this.props.onChange}
-            options={this.state.pokemon.map((option) => option.name_pokemon)}
- 
-            renderInput={(params) => (
-              <TextField {...params} label="pokemon" margin="normal"  />
-            )}
-          />
-          
-      </>
+      <div className="pokefinder">
+        <MyTooltip title={tooltip}  >
+          <button onClick={handleClick}> <img src={iconFind} /></button>
+        </MyTooltip>
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          disableClearable
+          onChange={this.props.onChange}
+          options={this.state.pokemon.map((option) => option.name_pokemon)}
+          renderInput={(params) => (
+            <TextField {...params} label="" margin="normal" placeholder="Search pokémon" />
+          )}
+        />
+
+      </ div>
     )
   }
 }
