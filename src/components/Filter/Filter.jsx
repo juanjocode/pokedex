9c4dc0ca26d4d7694f2fef01c0cc0f3b323c966a
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-24 10:12:24 -0500

    fix bug filter expanded menu collapsed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -23,22 +23,35 @@ export default function PokeFilter() {
 
     return (
         <div className="pokefilter">
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                >
-                    <button onClick={expandMenu}>    <img src={FilterImg} alt="filter" /> </button>
-                    <Typography className="acordion__title">Filter</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <div>
-                    acá va el contenido de esta joda
-                     </div>
-                </AccordionDetails>
-            </Accordion>
+            <button onClick={expandMenu}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                    >
+                        <img src={FilterImg} alt="filter" />
+                        <Typography className="acordion__title">Filter</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="pokefilter__content">
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                            <p>   acá va el contenido de esta joda</p>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
 
+            </button>
         </div>
     );
 
